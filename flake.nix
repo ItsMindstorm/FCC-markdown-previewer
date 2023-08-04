38b0d6bdafa037c254c50fa7693b9affcc078f15
yvess <yvess.straten@gmail.com>
2023-08-04 11:02:34 +0800

    Added dev shell

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # Made available on the CLI
+          nodejs_20
+        ];
+      };
+
+      # You could also define extra shells or packages here
+    };
+}
